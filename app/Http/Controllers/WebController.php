b81daaa7a6772f33bdbabf030f6ee60c87e692ed
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-03-27 09:58:22 +0700

    list data

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home(){
-        return view("welcome");
+        // lấy ra danh sách sản phẩm
+//        $products = Product::all();
+//        $products = Product::orderBy("id","desc")->limit(15)->get(); // collection
+        $products = Product::orderBy("id","desc")->paginate(15);
+        return view("welcome",compact('products')); // render
     }
 
     public function aboutUs(){
