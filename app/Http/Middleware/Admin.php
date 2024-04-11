974c70d6515194af00e570bd5e233e4663917f38
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-04-11 11:12:04 +0700

    authorize

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $current_user = auth()->user();
+        if($current_user->role != "ADMIN")
+            return abort(404);
+        return $next($request); // cho đi tiếp sang controller
+    }
+}
